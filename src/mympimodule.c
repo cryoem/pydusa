8a08fbb66f59d8dfc3ab6a74d15a537e70d6b349
adnan <adnan.ali@mpi-dortmund.mpg.de>
2020-03-25 20:20:52 +0100

    testing mpi_bcast function where it crashes   (test case 4)

--- src/mympimodule.c
+++ src/mympimodule.c
@@ -1173,7 +1173,7 @@ Py_ssize_t ln=0;
     result = (PyArrayObject *)PyArray_FromDims(1, dimensions, getptype(datatype));
     printf("result is %d", result->data);
 	aptr=(char*)(result->data);
-	printf("aptr values is %d" *aptr);
+	printf("aptr values is %d" (void*)aptr);
     ierr=MPI_Comm_rank((MPI_Comm)comm,&myid);
 #ifdef MPI2
     if(myid == root || root == MPI_ROOT) {
