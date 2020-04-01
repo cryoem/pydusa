399e78a2ac14b807d1ed4e5e8c9e498e4835d37a
adnan <adnan.ali@mpi-dortmund.mpg.de>
2020-04-01 15:13:53 +0200

    return result instead of result

--- src/mympimodule.c
+++ src/mympimodule.c
@@ -1174,8 +1174,8 @@ Py_ssize_t ln=0;
 			PyBytes_AsStringAndSize(input,&aptr,&ln);
 //			if (ln!=count) printf("lnc %d ct %d\n",ln,count);
 			ierr=MPI_Bcast(aptr, ln, (MPI_Datatype)datatype, root, (MPI_Comm)comm);
-			return result->data;
-//			return PyLong_FromLong((long)ierr);
+//			return result->data;
+			return PyLong_FromLong((long)aptr);
 		}
 		array = (PyArrayObject *) PyArray_ContiguousFromObject(input, getptype(datatype), 0, 3);
 		if (array == NULL)
